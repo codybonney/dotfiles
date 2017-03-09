f62d0c093fce79c3eb3ba44692ccaa445b72a916
Cody Bonney <me@codybonney.com>
2017-03-08 17:05:58 -0700

    updated polybar launch sequence

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,11 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 polybar top &
+echo "polybar top launched"
+sleep 0.5
 polybar right &
+echo "polybar right launched"
+sleep 0.5
 polybar left &
+echo "polybar left launched"
 
-echo "Bars launched..."
