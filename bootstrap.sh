2a4222d3d882bad1db37daa01f7b5500007b6472
Cody Bonney <me@codybonney.com>
2018-07-14 17:23:58 -0600

    add polybar

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 stow --verbose --target=$HOME bash
 stow --verbose --target=$HOME compton
+stow --verbose --target=$HOME polybar
 stow --verbose --target=$HOME rofi
 stow --verbose --target=$HOME vim
 stow --verbose --target=$HOME x
